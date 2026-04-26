1478538e12e793abb263da24855dacf53abfb397
bababue <>
2026-04-26 19:36:47 +0200

    Revert mason.lua

--- neovim/.config/nvim/lua/bababue/plugins/mason.lua
+++ neovim/.config/nvim/lua/bababue/plugins/mason.lua
@@ -1,15 +1,21 @@
 return {
-  "mason-org/mason-lspconfig.nvim",
-  opts = {
-    ensure_installed = {
-      "lua_ls",
-      "pyright",
-      "html",
-      "cssls"
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+        ensure_installed = {
+            "lua_ls",
+            "pyright",
+            "html",
+            "cssls"
+        }
+    },
+    dependencies = {
+        {
+            "mason-org/mason.nvim",
+            opts = {
+
+            }
+        },
+        "neovim/nvim-lspconfig"
     }
-  },
-  dependencies = {
-    "mason-org/mason.nvim",
-    "neovim/nvim-lspconfig"
-  }
 }
+
