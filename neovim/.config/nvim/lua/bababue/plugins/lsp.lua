ab3e8db0981f9496c235659e7cc8a486dd354cb8
bababue <>
2026-04-29 13:43:25 +0200

    Add nvim conf for LaTeX

---
+++ neovim/.config/nvim/lua/bababue/plugins/lsp.lua
@@ -0,0 +1,21 @@
+return {
+  "mason-org/mason-lspconfig.nvim",
+  opts = {
+    ensure_installed = {
+      "lua_ls",
+      "pyright",
+      "html",
+      "cssls",
+      "texlab"
+    }
+  },
+  dependencies = {
+    {
+      "mason-org/mason.nvim",
+      opts = {
+
+      }
+    },
+    "neovim/nvim-lspconfig"
+  }
+}
