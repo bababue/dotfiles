2598277b719a0ef14ad45c57bdbc673688a5cdc8
bababue <>
2026-04-30 19:06:16 +0200

    Treesitter fix

--- neovim/.config/nvim/lua/bababue/plugins/qualityoflife.lua
+++ neovim/.config/nvim/lua/bababue/plugins/qualityoflife.lua
@@ -4,7 +4,19 @@ return {
     config = function()
       require('mini.surround').setup()
       require('mini.comment').setup()
-      require('mini.pairs').setup()
+      require('mini.comment').setup()
+
+      local spec_treesitter = require('mini.ai').gen_spec.treesitter
+      require('mini.ai').setup({
+        custom_textobjects = {
+
+          F = spec_treesitter({ a = '@function.outer', i = '@function.inner' }),
+          o = spec_treesitter({
+            a = { '@conditional.outer', '@loop.outer' },
+            i = { '@conditional.inner', '@loop.inner' },
+          })
+        }
+      })
     end
   },
   {
--- neovim/.config/nvim/lua/bababue/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/bababue/plugins/treesitter.lua
@@ -1,151 +1,40 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
+    lazy = false,
     build = ":TSUpdate",
     dependencies = {
       "windwp/nvim-ts-autotag",
     },
-    branch = "main",
     config = function()
-      require("nvim-treesitter.config").setup({ -- enable syntax highlighting
-        install_dir = vim.fn.stdpath('data') .. '/site',
-        highlight = {
-          enable = true,
-        },
-        -- enable indentation
-        indent = { enable = true },
-        -- enable autotagging (w/ nvim-ts-autotag plugin)
-        autotag = {
-          enable = true,
-        },
-        -- ensure these language parsers are installed
-        ensure_installed = {
-          "json",
-          "javascript",
-          "yaml",
-          "toml",
-          "xml",
-          "html",
-          "css",
-          "markdown",
-          "markdown_inline",
-          "bash",
-          "lua",
-          "dockerfile",
-          "gitignore",
-          "vimdoc",
-          "sql",
-          "python"
-        },
-        auto_install = true
+      require("nvim-treesitter").setup({
+        install_dir = vim.fn.stdpath("data") .. "/site",
       })
+
+      require("nvim-treesitter").install({
+        "json", "javascript", "yaml", "toml",
+        "xml", "html", "css", "markdown",
+        "markdown_inline", "bash", "lua",
+        "dockerfile", "gitignore", "vimdoc",
+        "sql", "python",
+      })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "*" },
+        callback = function()
+          local ok = pcall(vim.treesitter.start)
+          if ok then
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+
+      require("nvim-ts-autotag").setup()
     end,
   },
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
     branch = "main",
-    config = function()
-      local select = require "nvim-treesitter-textobjects.select"
-      local move   = require "nvim-treesitter-textobjects.move"
-
-      -- ─── Text Objects ────────────────────────────────────────────────────────────
-
-      -- classes / definitions
-      vim.keymap.set({ "x", "o" }, "ac", function()
-        select.select_textobject("@class.outer", "textobjects")
-      end, { desc = "Around a class/definition" })
-
-      vim.keymap.set({ "x", "o" }, "ic", function()
-        select.select_textobject("@class.inner", "textobjects")
-      end, { desc = "Inside a class/definition" })
-
-      -- functions / methods
-      vim.keymap.set({ "x", "o" }, "af", function()
-        select.select_textobject("@function.outer", "textobjects")
-      end, { desc = "Around a function/method" })
-
-      vim.keymap.set({ "x", "o" }, "if", function()
-        select.select_textobject("@function.inner", "textobjects")
-      end, { desc = "Inside a function/method" })
-
-      -- comments  (note: 'gc' is also used by Comment.nvim as an operator)
-      vim.keymap.set({ "x", "o" }, "gc", function()
-        select.select_textobject("@comment.outer", "textobjects")
-      end, { desc = "A comment" })
-
-      -- arguments / list items
-      vim.keymap.set({ "x", "o" }, "ia", function()
-        select.select_textobject("@parameter.inner", "textobjects")
-      end, { desc = "An argument/list item" })
-
-      vim.keymap.set({ "x", "o" }, "aa", function()
-        select.select_textobject("@parameter.outer", "textobjects")
-      end, { desc = "An argument/list item (incl. trailing comma)" })
-
-      -- HTML-like tags
-      vim.keymap.set({ "x", "o" }, "at", function()
-        select.select_textobject("@tag.outer", "textobjects")
-      end, { desc = "Around an HTML-like tag" })
-
-      vim.keymap.set({ "x", "o" }, "it", function()
-        select.select_textobject("@tag.inner", "textobjects")
-      end, { desc = "Inside an HTML-like tag" })
-
-      -- ─── Motions ─────────────────────────────────────────────────────────────────
-
-      -- Go to next/previous method start:  ]m / [m
-      vim.keymap.set({ "n", "x", "o" }, "]m", function()
-        move.goto_next_start("@function.outer", "textobjects")
-      end, { desc = "Go to next method start" })
-
-      vim.keymap.set({ "n", "x", "o" }, "[m", function()
-        move.goto_previous_start("@function.outer", "textobjects")
-      end, { desc = "Go to previous method start" })
-
-      -- Go to next/previous method end:  ]M / [M
-      vim.keymap.set({ "n", "x", "o" }, "]M", function()
-        move.goto_next_end("@function.outer", "textobjects")
-      end, { desc = "Go to next method end" })
-
-      vim.keymap.set({ "n", "x", "o" }, "[M", function()
-        move.goto_previous_end("@function.outer", "textobjects")
-      end, { desc = "Go to previous method end" })
-
-      -- Go to next/previous section (class) start:  ]] / [[
-      vim.keymap.set({ "n", "x", "o" }, "]]", function()
-        move.goto_next_start("@class.outer", "textobjects")
-      end, { desc = "Go to next section start" })
-
-      vim.keymap.set({ "n", "x", "o" }, "[[", function()
-        move.goto_previous_start("@class.outer", "textobjects")
-      end, { desc = "Go to previous section start" })
-
-      -- Go to next/previous section end:  ][ / []
-      vim.keymap.set({ "n", "x", "o" }, "][", function()
-        move.goto_next_end("@class.outer", "textobjects")
-      end, { desc = "Go to next section end" })
-
-      vim.keymap.set({ "n", "x", "o" }, "[]", function()
-        move.goto_previous_end("@class.outer", "textobjects")
-      end, { desc = "Go to previous section end" })
-
-      -- Go to next/previous comment:  ]/ , ]* / [/ , [*
-      vim.keymap.set({ "n", "x", "o" }, "]/", function()
-        move.goto_next_start("@comment.outer", "textobjects")
-      end, { desc = "Go to next comment" })
-
-      vim.keymap.set({ "n", "x", "o" }, "]*", function()
-        move.goto_next_start("@comment.outer", "textobjects")
-      end, { desc = "Go to next comment" })
-
-      vim.keymap.set({ "n", "x", "o" }, "[/", function()
-        move.goto_previous_start("@comment.outer", "textobjects")
-      end, { desc = "Go to previous comment" })
-
-      vim.keymap.set({ "n", "x", "o" }, "[*", function()
-        move.goto_previous_start("@comment.outer", "textobjects")
-      end, { desc = "Go to previous comment" })
-    end,
-  }
+    lazy = false,
+  },
 }
