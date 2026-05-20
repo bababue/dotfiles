9d9dbfcb26b406ca318173a9431bf3f3a78f1ea9
bababue <>
2026-05-20 15:25:58 +0200

    Changes

--- neovim/.config/nvim/lua/bababue/plugins/obsidian.lua
+++ neovim/.config/nvim/lua/bababue/plugins/obsidian.lua
@@ -1,28 +1,6 @@
 return {
-	"obsidian-nvim/obsidian.nvim",
-	version = "*",
-	lazy = true,
-	ft = "markdown",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
+	"MeanderingProgrammer/render-markdown.nvim",
 	config = function()
-		vim.opt.conceallevel = 1
-		require("obsidian").setup({
-			legacy_commands = false,
-			sync = {
-				enabled = true,
-			},
-			frontmatter = { enabled = false },
-			workspaces = {
-				{
-					name = "personal",
-					path = "~/obsidian/Sync/",
-				},
-			},
-			completion = {
-				blink = true,
-			},
-		})
+		require("render-markdown").setup()
 	end,
 }
