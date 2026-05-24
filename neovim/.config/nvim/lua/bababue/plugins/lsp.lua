738c30f09358a9f77512bf61c1471bfc18506bc2
bababue <>
2026-05-24 19:03:20 +0200

    Formatting

--- neovim/.config/nvim/lua/bababue/plugins/lsp.lua
+++ neovim/.config/nvim/lua/bababue/plugins/lsp.lua
@@ -1,65 +1,65 @@
 return {
-  {
-    "mason-org/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "pyright",
-          "html",
-          "cssls",
-          "texlab",
-          "marksman",
-          "gopls"
-        },
-      })
+	{
+		"mason-org/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"pyright",
+					"html",
+					"cssls",
+					"texlab",
+					"marksman",
+					"gopls",
+					"clangd",
+				},
+			})
 
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = args.buf, desc = "LSP: Go to declaration" })
-          vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = args.buf, desc = "LSP: Go to definition" })
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = args.buf, desc = "LSP: Go to declaration" })
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = args.buf, desc = "LSP: Go to definition" })
 
-          vim.keymap.set(
-            "n",
-            "gR",
-            "<cmd>Telescope lsp_references<CR>",
-            { buffer = args.buf, desc = "LSP: Show references" }
-          )
-          vim.keymap.set(
-            "n",
-            "gi",
-            "<cmd>Telescope lsp_implementations<CR>",
-            { buffer = args.buf, desc = "LSP: Show implementations" }
-          )
-          vim.keymap.set(
-            "n",
-            "gt",
-            "<cmd>Telescope lsp_type_definitions<CR>",
-            { buffer = args.buf, desc = "LSP: Go to type definition" }
-          )
+					vim.keymap.set(
+						"n",
+						"gR",
+						"<cmd>Telescope lsp_references<CR>",
+						{ buffer = args.buf, desc = "LSP: Show references" }
+					)
+					vim.keymap.set(
+						"n",
+						"gi",
+						"<cmd>Telescope lsp_implementations<CR>",
+						{ buffer = args.buf, desc = "LSP: Show implementations" }
+					)
+					vim.keymap.set(
+						"n",
+						"gt",
+						"<cmd>Telescope lsp_type_definitions<CR>",
+						{ buffer = args.buf, desc = "LSP: Go to type definition" }
+					)
 
-          vim.keymap.set(
-            { "n", "v" },
-            "<leader>ca",
-            vim.lsp.buf.code_action,
-            { buffer = args.buf, desc = "LSP: Code action" }
-          )
-          vim.keymap.set("n", "<leader>gr", vim.lsp.buf.rename, { buffer = args.buf, desc = "LSP: Rename symbol" })
-        end,
-      })
-    end,
-    dependencies = {
-      {
-        "mason-org/mason.nvim",
-        opts = {},
-      },
-      "neovim/nvim-lspconfig",
-      "nvim-telescope/telescope.nvim",
-    },
-  },
-  {
-    "folke/lazydev.nvim",
-    ft = "lua",
-    opts = {},
-  },
+					vim.keymap.set(
+						{ "n", "v" },
+						"<leader>ca",
+						vim.lsp.buf.code_action,
+						{ buffer = args.buf, desc = "LSP: Code action" }
+					)
+					vim.keymap.set("n", "<leader>gr", vim.lsp.buf.rename, { buffer = args.buf, desc = "LSP: Rename symbol" })
+				end,
+			})
+		end,
+		dependencies = {
+			{
+				"mason-org/mason.nvim",
+				opts = {},
+			},
+			"neovim/nvim-lspconfig",
+		},
+	},
+	{
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {},
+	},
 }
